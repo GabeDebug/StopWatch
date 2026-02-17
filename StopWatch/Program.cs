4242a39e734cb1ff43c6e0fb29dde63484c049de
GabrielDebug <you@example.com>
2026-02-17 19:50:52 -0300

    Create Start

--- StopWatch/Program.cs
+++ StopWatch/Program.cs
@@ -1,12 +1,27 @@
-﻿using System;
-
-namespace MyNamespace
+﻿namespace StopWatch
 {
     class Program
     {
         static void Main()
         {
+            Start(6);
+        }
+        static void Menu()
+        {
             
+        }
+
+        static void Start(int time)
+        {
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
         }
     }
 }
