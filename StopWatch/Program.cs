6c8d7173264a8adfb563478434c6d091479dd014
GabrielDebug <you@example.com>
2026-02-17 20:20:53 -0300

    Create Menu

--- StopWatch/Program.cs
+++ StopWatch/Program.cs
@@ -4,11 +4,17 @@
     {
         static void Main()
         {
-            Start(6);
+            Menu();
         }
         static void Menu()
         {
-            
+            Console.Clear();
+            Console.WriteLine("S = Segundos => 10s  = 10 Segundos:");
+            Console.WriteLine("M = Minutos => 1m = 1 minuto:");
+            Console.WriteLine("0 = Sair:");
+            Console.WriteLine("Quanto tempo deseja Contar?");
+
+            string data = Console.ReadLine().ToLower();
         }
 
         static void Start(int time)
@@ -22,6 +28,9 @@
                 Console.WriteLine(currentTime);
                 Thread.Sleep(1000);
             }
+            Console.Clear();
+            Console.WriteLine("StopWatch finalizado");
+            Thread.Sleep(2500); 
         }
     }
 }
